a98eca5b5fd7c3db3e9b28e9173ce69f4db44e95
Ross Root <git@rroot.me>
2025-10-24 17:14:48 -0700

    scaffold movies singleton and website

---
+++ movies/movies.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var inventory: Dictionary = {
+	'001': {
+		'title': 'BAWS II',
+		'genre': 'HORROR',
+		'status': 'STOCKED',
+		'location': 'HOR-SHELF-02',
+		'reviews': [
+			{
+				'user': '@JDAWG79',
+				'review': 'BAWS II IS WHERE IT IS AT YO!!',
+				'positive': true
+			}
+		]
+	},
+	'002': {
+		'title': 'THE BOTENOOK',
+		'genre': 'ROMANCE',
+		'status': 'UNSTOCKED',
+		'location': 'REWIND-DESK'
+	},
+}
