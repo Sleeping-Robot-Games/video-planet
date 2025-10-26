04f0b15630087f6087fd05f42543e90fb2f9168f
Bryan Reed <bryan87reed@gmail.com>
2025-10-25 21:07:23 -0700

    adds UI sound to title screen sliders

--- title/title.gd
+++ title/title.gd
@@ -25,25 +25,30 @@ func _on_master_volume_value_changed(value: float) -> void:
 	var mute = value == -20
 	AudioServer.set_bus_mute(master_bus_idx, mute)
 	AudioServer.set_bus_volume_db(master_bus_idx, value)
+	a.play_sfx('menu_select')
 
 func _on_music_volume_value_changed(value: float) -> void:
 	var mute = value == -20
 	AudioServer.set_bus_mute(music_bus_idx, mute)
 	AudioServer.set_bus_volume_db(music_bus_idx, value)
+	a.play_sfx('menu_select')
 	
 func _on_sfx_volume_value_changed(value: float) -> void:
 	var mute = value == -20
 	AudioServer.set_bus_mute(sfx_bus_idx, mute)
 	AudioServer.set_bus_volume_db(sfx_bus_idx, value)
+	a.play_sfx('menu_select')
 
 
 func _on_ambience_volume_value_changed(value: float) -> void:
 	var mute = value == -20
 	AudioServer.set_bus_mute(ambience_bus_idx, mute)
 	AudioServer.set_bus_volume_db(ambience_bus_idx, value)
+	a.play_sfx('menu_select')
 
 
 func _on_ui_volume_value_changed(value: float) -> void:
 	var mute = value == -20
 	AudioServer.set_bus_mute(ui_bus_idx, mute)
 	AudioServer.set_bus_volume_db(ui_bus_idx, value)
+	a.play_sfx('menu_select')
